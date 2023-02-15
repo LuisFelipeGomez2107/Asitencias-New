ea548ba958ab2b2c017f9b7ff2f9b2499ef08d0d
erick260392 <erickliconamorales2016@gmail.com>
2023-02-15 10:17:26 -0600

    bugCameraUserInstalador

--- resources/views/dashboardInstalador/dashboard.blade.php
+++ resources/views/dashboardInstalador/dashboard.blade.php
@@ -146,22 +146,22 @@
             </div>
         </main>
         <!-- Optional JavaScript; choose one of the two! -->
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script> --}}
-    @if(isset($_GET['success']) && $valor==1 )
-    <script>
-      $("#verify").val("result")
-      $("#succes").removeClass('d-none')
-      $("#twoVery").val(1);
-      $(".swal2-backdrop-show").remove();
-    </script>
-    @elseif(isset($_GET['success']) && $valor==2)
-    @else
-    <script src="{{ asset('js/modules.js')}}" type="module"></script>
-   
-    @endif
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+        @if (isset($_GET['success']) && $valor == 1)
+            <script>
+                $("#verify").val("result")
+                $("#succes").removeClass('d-none')
+                $("#twoVery").val(1);
+                $(".swal2-backdrop-show").remove();
+            </script>
+        @elseif(isset($_GET['success']) && $valor == 2)
+        @else
+            <script src="{{ asset('js/modules.js')}}" type="module"></script>
+        @endif
     </section>
 
 
